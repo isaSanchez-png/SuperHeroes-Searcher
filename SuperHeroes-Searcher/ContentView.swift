b27ba260fc1dd6611172d6b1618b6197ad303a1d
isaSanchez-png <isabela.sanchez.mol@gmail.com>
2026-06-10 17:50:50 -0500

    Initial Commit

---
+++ SuperHeroes-Searcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperHeroes-Searcher
+//
+//  Created by Isa on 10/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperHeroes-Searcher/SuperHeroes_SearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroes_SearcherApp.swift
+//  SuperHeroes-Searcher
+//
+//  Created by Isa on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroes_SearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
